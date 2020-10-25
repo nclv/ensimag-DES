7c144ebf684144e8fb9b49edcd8dd0032c0f8844
Samy <samy.vincent10@gmail.com>
2020-10-25 09:08:41 +0100

    map gui

--- src/TestGUI.java
+++ src/TestGUI.java
@@ -1,18 +1,31 @@
 import java.awt.Color;
 import java.io.FileNotFoundException;
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.zip.DataFormatException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import game.DonneesSimulation;
+import game.NatureTerrain;
 import gui.GUISimulator;
-import gui.Rectangle;
+import gui.ImageElement;
 import gui.Simulable;
 import io.LecteurDonnees;
 
 public class TestGUI {
     public static void main(String[] args) {
         DonneesSimulation donneesSimulation = getDonneesSimulation(args);
-        GUISimulator gui = new GUISimulator(donneesSimulation.getCarte().getNbLignes(), donneesSimulation.getCarte().getNbColonnes(), Color.BLACK);
-        Simulateur simulateur = new Simulateur(gui, Color.decode("#f2ff28"), donneesSimulation);
+
+        int sizeFactor = 60;  // à adpater à son écran
+        GUISimulator gui = new GUISimulator(
+            donneesSimulation.getCarte().getNbLignes() * sizeFactor, 
+            donneesSimulation.getCarte().getNbColonnes() * sizeFactor, 
+            Color.BLACK
+        );
+
+        Simulateur simulateur = new Simulateur(gui, sizeFactor, donneesSimulation);
     }
 
     public static DonneesSimulation getDonneesSimulation(String[] args) {
@@ -33,10 +46,23 @@ public class TestGUI {
     }
 }
 
+// ImageObserver is an interface
 class Simulateur implements Simulable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LecteurDonnees.class);
+
     private GUISimulator gui;
-    private Color simulateurColor;
+    private int sizeFactor;
     private DonneesSimulation donneesSimulation;
+
+
+    static final EnumMap<NatureTerrain, String> ressourcesMap = new EnumMap<NatureTerrain, String>(Map.of(
+        NatureTerrain.EAU, "eau.png", 
+        NatureTerrain.FORET, "foret.png", 
+        NatureTerrain.HABITAT, "habitat.png",
+        NatureTerrain.ROCHE, "roche.png", 
+        NatureTerrain.TERRAIN_LIBRE, "terrain_libre.png"
+        )
+    );
 
     /**
      * Crée un Simulateur et le dessine.
@@ -46,11 +72,13 @@ class Simulateur implements Simulable {
      *              Simulable.
      * @param color la couleur du simulateur
      */
-    public Simulateur(GUISimulator gui, Color simulateurColor, DonneesSimulation donneesSimulation) {
+    public Simulateur(GUISimulator gui, int sizeFactor, DonneesSimulation donneesSimulation) {
         this.gui = gui;
+        LOGGER.info("GUI de dimensions {}x{}", gui.getPanelHeight(), gui.getPanelWidth());
+
         gui.setSimulable(this);
 
-        this.simulateurColor = simulateurColor;
+        this.sizeFactor = sizeFactor;
         this.donneesSimulation = donneesSimulation;
 
         drawMap();
@@ -59,6 +87,35 @@ class Simulateur implements Simulable {
     private void drawMap() {
         gui.reset(); // clear the window
 
-        gui.addGraphicalElement(new Rectangle(x + 30, y, invaderColor, invaderColor, 10));
+        Map<Integer, NatureTerrain> map = this.donneesSimulation.getCarte().getMap();
+        int nbLignes = this.donneesSimulation.getCarte().getNbLignes();
+
+        for (Map.Entry<Integer, NatureTerrain> tile : map.entrySet()) {
+            int x = tile.getKey() % nbLignes;
+            int y = tile.getKey() / nbLignes;
+
+            LOGGER.info("Affichage de la case ({}, {}) de type {}", x, y, tile.getValue());
+
+            gui.addGraphicalElement(new ImageElement(
+                x * this.sizeFactor, 
+                y * this.sizeFactor,
+                "src\\ressources\\" + ressourcesMap.get(tile.getValue()), 
+                this.sizeFactor, this.sizeFactor, 
+                null)
+            );
+        }
+
+    }
+
+    @Override
+    public void next() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void restart() {
+        // TODO Auto-generated method stub
+
     }
 }
