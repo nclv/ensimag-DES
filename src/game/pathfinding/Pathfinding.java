52d1bc206e7b9d30a8df3e01b5ebbf8bf5ca4350
NicovincX2 <nicolas.vincent100@gmail.com>
2020-11-06 11:19:02 +0100

    pathfinding refactoring

--- src/game/pathfinding/Pathfinding.java
+++ src/game/pathfinding/Pathfinding.java
@@ -5,5 +5,14 @@ import java.util.LinkedList;
 import game.robots.Robot;
 
 public abstract class Pathfinding {
+    /**
+     * Calcule le plus court chemin pour le robot entre src et dest
+     * 
+     * @param robot
+     * @param src
+     * @param dest
+     * @return suite de positions
+     * @throws IllegalStateException if there is no path
+     */
     public abstract LinkedList<Integer> shortestWay(Robot robot, int src, int dest) throws IllegalStateException;
 }
